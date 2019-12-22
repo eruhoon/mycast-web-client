b3efd247b2a56f981a2718dc94c16a45ed4c6c06
Whimsicott <eruhoon@gmail.com>
2019-12-22 19:23:15 +0900

    add view binding for StreamListComponent

--- src/app/models/stream/MutableStream.ts
+++ src/app/models/stream/MutableStream.ts
@@ -3,42 +3,96 @@ import { Stream } from './Stream';
 export class MutableStream implements Stream {
 
     private mPlatform: string;
+    private mKeyId: string;
+    private mIcon: string;
+    private mThumbnail: string;
+    private mTitle: string;
+    private mDescription: string;
+    private mUrl: string;
+    private mViewer: number;
+    private mOnAir: boolean;
 
     public constructor() {
-
+        this.mPlatform = '';
+        this.mKeyId = '';
+        this.mIcon = '';
+        this.mThumbnail = '';
+        this.mTitle = '';
+        this.mDescription = '';
+        this.mUrl = '';
+        this.mViewer = 0;
+        this.mOnAir = false;
     }
 
-    setPlatform(platform: string) {
-        this.mPlatform = platform;
-    }
-    getPlatform(): string {
+    public getPlatform(): string {
         return this.mPlatform;
     }
-    getKeyId(): string {
-        return '';
-    }
-    getIcon(): string {
-        return '';
+
+    public setPlatform(platform: string): void {
+        this.mPlatform = platform;
     }
 
-    getThumbnail(): string {
-        return '';
+    public getKeyId(): string {
+        return this.mKeyId;
     }
 
-    getTitle(): string {
-        return '';
-    }
-    getDescription(): string {
-        return '';
-    }
-    getUrl(): string {
-        return '';
-    }
-    getViewer(): number {
-        return 0;
-    }
-    isOnAir(): boolean {
-        return false;
+    public setKeyId(keyId: string): void {
+        this.mKeyId = keyId;
     }
 
+    public getIcon(): string {
+        return this.mIcon;
+    }
+
+    public setIcon(icon: string): void {
+        this.mIcon = icon;
+    }
+
+    public getThumbnail(): string {
+        return this.mThumbnail;
+    }
+
+    public setThumbnail(thumbnail: string): void {
+        this.mThumbnail = thumbnail;
+    }
+
+    public getTitle(): string {
+        return this.mTitle;
+    }
+
+    public setTitle(title: string): void {
+        this.mTitle = title;
+    }
+
+    public getDescription(): string {
+        return this.mDescription;
+    }
+
+    public setDescription(description: string): void {
+        this.mDescription = description;
+    }
+
+    public getUrl(): string {
+        return this.mUrl;
+    }
+
+    public setUrl(url: string): void {
+        this.mUrl = url;
+    }
+
+    public getViewer(): number {
+        return this.mViewer;
+    }
+
+    public setViewer(viewer: number): void {
+        this.mViewer = viewer;
+    }
+
+    public isOnAir(): boolean {
+        return this.mOnAir;
+    }
+
+    public setOnAir(onAir: boolean): void {
+        this.mOnAir = onAir;
+    }
 }
--- src/app/views/main-page/stream-list/stream-list.component.ts
+++ src/app/views/main-page/stream-list/stream-list.component.ts
@@ -2,6 +2,7 @@ import Axios from 'axios';
 import { Stream } from 'src/app/models/stream/Stream';
 
 import { Component, OnInit } from '@angular/core';
+import { MutableStream } from 'src/app/models/stream/MutableStream';
 
 @Component({
   selector: 'app-stream-list',
@@ -10,18 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StreamListComponent implements OnInit {
 
-  private mStreams = [1, 2, 3];
-  constructor() { }
+  private mStreams: Stream[];
+
+  constructor() {
+    this.mStreams = [];
+  }
 
   public async ngOnInit() {
-    let ttt = await Axios.get('https://mycast.xyz:9000/local/');
-    console.log(ttt);
+    //let ttt = await Axios.get('https://mycast.xyz:9000/local/');
+    this.mStreams = StreamListComponent.createStreams();
   }
 
   public getStreams(): Stream[] {
-    const streams = [];
+    /*const streams = [];
     streams.push(1);
-    streams.push(2);
+    streams.push(2);*/
+    return this.mStreams;
+  }
+
+  private static createStreams(): Stream[] {
+    const streams: Stream[] = [];
+    const stream = new MutableStream();
+    stream.setIcon('https://i.imgur.com/KUbNw6O.gif');
+    stream.setThumbnail('https://i.imgur.com/5hgzZcl.gif');
+    streams.push(stream);
+    const stream2 = new MutableStream();
+    stream2.setIcon('');
+    streams.push(stream2);
     return streams;
   }
 }

9b1b4c9b0b87d1c365d600aed30915dd141e9b55
엘풍 <eruhoon@gmail.com>
2019-12-25 22:51:03 +0900

    update example

--- src/app/models/stream/MockStreamListLoader.ts
+++ src/app/models/stream/MockStreamListLoader.ts
@@ -1,7 +1,7 @@
-import { StreamListLoader } from './StreamListLoader';
 import { OnLoadCallback } from '../loader/AsyncLoader';
-import { Stream } from './Stream';
 import { MutableStream } from './MutableStream';
+import { Stream } from './Stream';
+import { StreamListLoader } from './StreamListLoader';
 
 export class MockStreamListLoader implements StreamListLoader {
 
@@ -12,7 +12,7 @@ export class MockStreamListLoader implements StreamListLoader {
         shiguruna.setIcon('https://i.imgur.com/tXiHAIc.gif');
         shiguruna.setThumbnail('https://i.imgur.com/5ndSAT7.jpg');
         shiguruna.setTitle('시구르나');
-        shiguruna.setKeyId('kandelion');
+        shiguruna.setKeyId('tjdwhd613');
         shiguruna.setUrl('http://mycast.xyz/home/stream/local/9');
         shiguruna.setViewer(1);
         streams.push(shiguruna);
@@ -21,7 +21,7 @@ export class MockStreamListLoader implements StreamListLoader {
         megumin.setIcon('https://i.imgur.com/KUbNw6O.gif');
         megumin.setThumbnail('https://i.imgur.com/5hgzZcl.gif');
         megumin.setOnAir(true);
-        megumin.setKeyId('backtothearmy');
+        megumin.setKeyId('gksmf1677');
         megumin.setTitle('ᎷᏋᎶᏬᎷᎥᏁ');
         megumin.setDescription('ᎷᏋᎶᏬᎷᎥᏁ의 방송 [공용채널]');
         megumin.setViewer(5);
--- src/app/views/main-page/stream-viewer/stream-viewer.component.ts
+++ src/app/views/main-page/stream-viewer/stream-viewer.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Stream } from 'src/app/models/stream/Stream';
 
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+declare var Clappr: any;
 declare var RTMP: any;
 
 @Component({
@@ -32,10 +34,10 @@ export class StreamViewerComponent implements OnInit, OnChanges {
   private getPlayer() {
     if (this.mPlayer === null) {
       this.mPlayer = new Clappr.Player({
-        parentId: "#player",
+        parentId: '#player',
         width: '100%',
         height: '100%',
-        plugins: { 'playback': [RTMP] },
+        plugins: { playback: [RTMP] },
         rtmpConfig: {
           swfPath: './assets/clappr/player/RTMP.swf',
           scaling: 'stretch',
@@ -43,20 +45,20 @@ export class StreamViewerComponent implements OnInit, OnChanges {
           bufferTime: 1,
           startLevel: 0,
           switchRules: {
-            "SufficientBandwidthRule": {
-              "bandwidthSafetyMultiple": 1.15,
-              "minDroppedFps": 2
+            SufficientBandwidthRule: {
+              bandwidthSafetyMultiple: 1.15,
+              minDroppedFps: 2
             },
-            "InsufficientBufferRule": {
-              "minBufferLength": 2
+            InsufficientBufferRule: {
+              minBufferLength: 2
             },
-            "DroppedFramesRule": {
-              "downSwitchByOne": 10,
-              "downSwitchByTwo": 20,
-              "downSwitchToZero": 24
+            DroppedFramesRule: {
+              downSwitchByOne: 10,
+              downSwitchByTwo: 20,
+              downSwitchToZero: 24
             },
-            "InsufficientBandwidthRule": {
-              "bitrateMultiplier": 1.15
+            InsufficientBandwidthRule: {
+              bitrateMultiplier: 1.15
             }
           }
         },

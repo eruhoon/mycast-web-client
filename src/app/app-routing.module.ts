25e9f5694a0e73783e53b0dac03a3ac0815836fe
엘풍 <eruhoon@gmail.com>
2019-12-29 22:58:51 +0900

    update AppRoute

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ChatListComponent } from './views/chat/chat-list/chat-list.component';
+import { MainPageComponent } from './views/main-page/main-page.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

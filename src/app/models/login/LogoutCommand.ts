3bbd1aa6b014daf2911d4a67a60ce263c2076c73
엘풍 <eruhoon@gmail.com>
2020-01-04 19:18:15 +0900

    add LogoutCommand

---
+++ src/app/models/login/LogoutCommand.ts
@@ -0,0 +1,28 @@
+import Axios from 'axios';
+import * as qs from 'querystring';
+
+export class LogoutCommand {
+
+    private mSessionId: string;
+
+    public constructor(sessionId: string) {
+        this.mSessionId = sessionId;
+    }
+
+    public execute() {
+        const url = 'http://mycast.xyz:3000/auth';
+        const request = Axios.delete(url);
+
+        request.then(res => {
+            console.log(res.data);
+        }).catch(reason => {
+            console.log(reason);
+        });
+    }
+}
+
+type LogoutResponse = {
+    result: boolean,
+    hash: string,
+    message: string,
+};

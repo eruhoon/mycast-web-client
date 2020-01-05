46838e75c91299ffd46d22e4f2eaa602de312096
Whimsicott <eruhoon@gmail.com>
2020-01-05 12:26:34 +0900

    add keyboard action in LoginPage

---
+++ src/app/models/common/keyboard/Keyboard.ts
@@ -0,0 +1,3 @@
+export class Keyboard {
+    public static readonly KEY_ENTER: number = 13;
+}
--- src/app/views/login/login-page/login-page.component.ts
+++ src/app/views/login/login-page/login-page.component.ts
@@ -1,7 +1,8 @@
 import { LoginCommand } from 'src/app/models/login/LoginCommand';
 
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
+import { Keyboard } from 'src/app/models/common/keyboard/Keyboard';
 
 @Component({
   selector: 'app-login-page',
@@ -12,14 +13,41 @@ export class LoginPageComponent implements OnInit {
 
   protected mLoginForm: LoginForm;
 
-  constructor(private mRouter: Router) {
+  constructor(
+    private mRouter: Router,
+    private mRenderer: Renderer2) {
+
     this.mLoginForm = { id: '', pw: '' };
+
+    console.log(this.mRenderer);
   }
 
   ngOnInit() {
   }
 
-  protected onSubmit() {
+  protected onIdKeyPress(event: KeyboardEvent) {
+    if (event.keyCode === Keyboard.KEY_ENTER) {
+      this.setFocusPassword();
+    }
+  }
+
+  protected onPasswordKeyPress(event: KeyboardEvent) {
+    if (event.keyCode === Keyboard.KEY_ENTER) {
+      this.requestLogin();
+    }
+  }
+
+  protected onSubmit(): void {
+    this.requestLogin();
+  }
+
+  private setFocusPassword() {
+    const passwordInput = this.mRenderer.selectRootElement('#pw');
+    console.log(passwordInput);
+    passwordInput.focus();
+  }
+
+  private requestLogin(): void {
     const login = new LoginCommand(this.mLoginForm.id, this.mLoginForm.pw);
     login.onSuccess(() => {
       this.mRouter.navigate(['/']);

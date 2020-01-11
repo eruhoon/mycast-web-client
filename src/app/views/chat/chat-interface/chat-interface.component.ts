79d9f657aafd05e2f4d16a3d79443015fcabd474
Whimsicott <eruhoon@gmail.com>
2020-01-11 14:51:41 +0900

    add onKeyPress in ChatInterface

--- src/app/views/chat/chat-interface/chat-interface.component.ts
+++ src/app/views/chat/chat-interface/chat-interface.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
+import { Keyboard } from 'src/app/models/common/keyboard/Keyboard';
 
 @Component({
   selector: 'chat-interface',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatInterfaceComponent implements OnInit {
 
+  @ViewChild('inputBox', { static: false })
+  private mInputBox: ElementRef<HTMLInputElement>;
+
+  @Input() chatText: string;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  protected onKeyPress(event: KeyboardEvent): void {
+    if (event.keyCode === Keyboard.KEY_ENTER) {
+      this.mInputBox.nativeElement.value = '';
+    }
+  }
 }

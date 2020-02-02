daec73f6c851343d30e5411aab3193148c422d10
엘풍 <eruhoon@gmail.com>
2020-02-03 00:05:21 +0900

    add auth service

---
+++ src/app/services/auth/auth.guard.ts
@@ -0,0 +1,16 @@
+import { Observable } from 'rxjs';
+
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+
+}

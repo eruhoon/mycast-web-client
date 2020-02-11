3dd873873c15ae030c8e1b652219d074204ea4e6
엘풍 <eruhoon@gmail.com>
2020-02-12 01:53:46 +0900

    Remove Service from ChatPageComponent

--- src/app/views/chat/chat-page/chat-page.component.ts
+++ src/app/views/chat/chat-page/chat-page.component.ts
@@ -1,28 +1,19 @@
-import { Chat } from 'src/app/models/chat/Chat';
 import { ChatNetworkModel } from 'src/app/models/network/ChatNetworkModel';
-import { CurrentChatService } from 'src/app/services/chat/current-chat.service';
 import { MainService } from 'src/app/services/main/main.service';
 
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'chat-page',
   templateUrl: './chat-page.component.html',
   styleUrls: ['./chat-page.component.scss']
 })
-export class ChatPageComponent implements OnInit {
+export class ChatPageComponent {
 
-  private mCurrentChatService: CurrentChatService;
   private mChatNetwork: ChatNetworkModel;
 
-  public constructor(
-    mainService: MainService, currentChatService: CurrentChatService) {
-
-    this.mCurrentChatService = currentChatService;
+  public constructor(mainService: MainService) {
     this.mChatNetwork = mainService.getChatNework();
-  }
-
-  public ngOnInit() {
   }
 
   public onChatInput(rawChat: string): void {
@@ -31,9 +22,5 @@ export class ChatPageComponent implements OnInit {
 
   public onChatEntryIconSelect(icon: string) {
     this.mChatNetwork.chat(icon);
-  }
-
-  public getCurrentChats(): Chat[] {
-    return this.mCurrentChatService.getChats();
   }
 }

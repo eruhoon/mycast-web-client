da51bd5668fda5a70776400473e06f4caf6bd271
엘풍 <eruhoon@gmail.com>
2020-02-25 22:31:39 +0900

    add ModifyProfileModal

---
+++ src/app/views/main-page/modify-profile-modal/modify-profile-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'modify-profile-modal',
+  templateUrl: './modify-profile-modal.component.html',
+  styleUrls: ['./modify-profile-modal.component.scss']
+})
+export class ModifyProfileModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

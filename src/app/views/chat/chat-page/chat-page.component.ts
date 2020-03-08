5cfdd3126787097605358b90ca9ed29323164e97
Whimsicott <eruhoon@gmail.com>
2020-03-08 22:10:57 +0900

    add image paste interface

--- src/app/views/chat/chat-page/chat-page.component.ts
+++ src/app/views/chat/chat-page/chat-page.component.ts
@@ -23,6 +23,46 @@ export class ChatPageComponent {
     return this.mChatService.isChatUserListShow();
   }
 
+  public onPaste(event: ClipboardEvent): void {
+    if (!event.clipboardData || !event.clipboardData.items) return;
+    const items = event.clipboardData.items;
+
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i];
+      let blob: File | null = null;
+      if (item.type.indexOf('image') === 0) {
+        blob = item.getAsFile();
+        console.log(blob);
+      }
+    }
+    //let items = event.originalEvent['clipboardData'].items;
+
+    // let blob: File = null;
+    // for (let item of items) {
+    //   if (item.type.indexOf('image') === 0) {
+    //     blob = item.getAsFile(new Date().getTime() + '.png');
+    //   }
+    // }
+
+    // if (!blob) return;
+
+    // const formData = new FormData();
+    // formData.append('image', blob);
+    // let url = 'http://mycast.xyz:8001/cache';
+    // $.ajax({
+    //   contentType: false,
+    //   data: formData,
+    //   processData: false,
+    //   type: 'POST',
+    //   url: url,
+    // }).done(data => {
+    //   if (!data) return;
+    //   let imageUri = data;
+    //   let dialog = new ClipboardImageDialog({ imageUri });
+    //   dialog.open();
+    // });
+  }
+
   public onChatInput(rawChat: string): void {
     this.mChatNetwork.chat(rawChat);
   }

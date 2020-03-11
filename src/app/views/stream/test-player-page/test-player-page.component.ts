7387ae56442509ba5aac096b247d557f646f9f44
엘풍 <eruhoon@gmail.com>
2020-03-11 22:29:04 +0900

    apply TestPlayerPage

--- src/app/views/stream/test-player-page/test-player-page.component.ts
+++ src/app/views/stream/test-player-page/test-player-page.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+declare class Plyr {
+  constructor(element: any, option: any);
+  public on(event: string, callback?: any): void;
+}
+declare var flvjs: any;
 
 @Component({
   selector: 'app-test-player-page',
@@ -7,9 +13,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestPlayerPageComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('player', { static: true })
+  public mPlayerView: ElementRef<HTMLVideoElement>;
+
+  private mPlyr: Plyr;
+  private mPlayer: any;
+
+  public constructor() {
+  }
 
   ngOnInit() {
+    const element = this.mPlayerView.nativeElement;
+    if (flvjs.isSupported()) {
+      // tslint:disable-next-line: no-unused-expression
+      const plyr = new Plyr(element, {
+        speed: {
+          selected: 1,
+          options: [0.5, 0.75, 1, 1.25, 1.5, 1.75, 2]
+        },
+        clickToPlay: false,
+        controls: [
+          'play-large', 'play', 'mute',
+          'volume', 'pip', 'airplay', 'fullscreen'
+        ]
+      });
+
+      this.mPlayer = flvjs.createPlayer({
+        enableWorker: false,
+        lazyLoadMaxDuration: 3 * 60,
+        type: 'flv',
+        isLive: true,
+        url: 'http://parasite.banjai.tv:8080/live/mcc1.flv'
+      });
+
+      plyr.on('ready', (event: any) => {
+        console.log(event);
+        this.mPlayer.play();
+      });
+
+      this.mPlayer.attachMediaElement(this.mPlayerView.nativeElement);
+
+      this.mPlayer.load();
+    } else {
+      console.log('not support');
+    }
   }
 
 }

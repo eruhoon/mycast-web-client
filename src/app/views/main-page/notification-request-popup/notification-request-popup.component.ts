6e7b630f4aa79dcea6b8f9506746c0ca9e1bc3ce
Whimsicott <eruhoon@gmail.com>
2020-03-15 14:24:14 +0900

    apply toast in NotificationRequestPopup

--- src/app/views/main-page/notification-request-popup/notification-request-popup.component.ts
+++ src/app/views/main-page/notification-request-popup/notification-request-popup.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NotificationService } from 'src/app/services/notification/notification.service';
 import { User } from 'src/app/models/user/User';
 import { MainService } from 'src/app/services/main/main.service';
+import { ToastService } from 'src/app/services/notification/toast.service';
 
 @Component({
   selector: 'notification-request-popup',
@@ -12,12 +13,15 @@ export class NotificationRequestPopupComponent implements OnInit {
 
   private mMainService: MainService;
   private mNotificationService: NotificationService;
+  private mToastService: ToastService;
 
   public constructor(
     mainService: MainService,
-    notificationService: NotificationService) {
+    notificationService: NotificationService,
+    toastService: ToastService) {
     this.mMainService = mainService;
     this.mNotificationService = notificationService;
+    this.mToastService = toastService;
   }
 
   public ngOnInit() {
@@ -44,6 +48,7 @@ export class NotificationRequestPopupComponent implements OnInit {
       return;
     }
     this.mMainService.notify(target.getHash());
+    this.mToastService.toast(`'${target.getName()}'님에게 호출했어요 !`);
   }
 
   public clearTarget(): void {

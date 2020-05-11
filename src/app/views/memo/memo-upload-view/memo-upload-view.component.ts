126d7017ec4676514709a464f64f113ed8ba08cc
Whimsicott <eruhoon@gmail.com>
2020-05-12 00:54:34 +0900

    add view param in MemoUploadView

--- src/app/views/memo/memo-upload-view/memo-upload-view.component.ts
+++ src/app/views/memo/memo-upload-view/memo-upload-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from 'src/app/services/profile/profile.service';
+import { SessionStorage } from 'src/app/models/storage/SessionStorage';
 
 @Component({
   selector: 'memo-upload-view',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MemoUploadViewComponent implements OnInit {
 
-  constructor() { }
+  private mService: ProfileService;
 
-  ngOnInit() {
+  private mIcon: string;
+  private mName: string;
+  private mDateString: string;
+
+  public constructor(service: ProfileService) {
+    this.mService = service;
+    this.mIcon = '';
+    this.mName = '';
+    this.mDateString = '';
   }
 
+  public ngOnInit() {
+    console.log(SessionStorage.getInstance().getPrivateKey());
+    this.mIcon = this.mService.getProfileIcon();
+    this.mName = this.mService.getName();
+    this.mDateString = MemoUploadViewComponent.getDateString(new Date());
+  }
+
+  public getIcon(): string {
+    return this.mIcon;
+  }
+
+  public getName(): string {
+    return this.mName;
+  }
+
+  public getDateString(): string {
+    return this.mDateString;
+  }
+
+  private static getDateString(dateObj: Date): string {
+    const month = dateObj.getMonth() + 1;
+    const date = dateObj.getDate();
+    return `${month}월 ${date}일`;
+  }
 }

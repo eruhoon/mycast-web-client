499972aab881e576e552b9f43811711a41748091
Whimsicott <eruhoon@gmail.com>
2020-05-16 01:57:50 +0900

    bind current stream in ModifyStreamModal

--- src/app/views/main-page/modify-stream-modal/modify-stream-modal.component.ts
+++ src/app/views/main-page/modify-stream-modal/modify-stream-modal.component.ts
@@ -9,13 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModifyStreamModalComponent implements OnInit {
 
+  private mPlatforms: PlatformType[];
   private mProfileService: ProfileService;
+  private mCurrentPlatform: string;
 
   public constructor(profileService: ProfileService) {
+    this.mPlatforms = [
+      { id: 'local', name: '로컬서버', src: '/assets/image/stream/mycast.png' },
+      { id: 'totoro', name: '이웃서버', src: '/assets/image/stream/totoro.png' },
+      { id: 'afreeca', name: '아프리카', src: '/assets/image/stream/afreeca.png' },
+      { id: 'twitch', name: '트위치', src: '/assets/image/stream/twitch.png' },
+      { id: 'mixer', name: '믹서', src: '/assets/image/stream/mixer.png' },
+    ];
     this.mProfileService = profileService;
+    this.mCurrentPlatform = 'local';
   }
 
-  ngOnInit() {
+  public ngOnInit() {
+    this.mProfileService.loadStream().then(() => {
+      this.mCurrentPlatform = this.mProfileService.getStreamPlatform();
+    });
+  }
+
+  public getPlatforms(): PlatformType[] { return this.mPlatforms; }
+
+  public isCurrentPlatform(platform: PlatformType): boolean {
+    return this.mCurrentPlatform === platform.id;
+  }
+
+  public getCurrentPlatform(): string {
+    return this.mCurrentPlatform;
   }
 
   public close() {
@@ -23,3 +46,5 @@ export class ModifyStreamModalComponent implements OnInit {
   }
 
 }
+
+type PlatformType = { id: string, name: string, src: string };

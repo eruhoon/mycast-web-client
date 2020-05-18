ceeda503e01fd7ff7e8b40a5b06a0d740a3a8900
엘풍 <eruhoon@gmail.com>
2020-05-18 21:56:40 +0900

    temporary implement of LolUserPack

--- src/app/views/chat/cp/lol-user-pack/lol-user-pack.component.ts
+++ src/app/views/chat/cp/lol-user-pack/lol-user-pack.component.ts
@@ -9,11 +9,42 @@ import { ChatPack } from '../ChatPack';
 })
 export class LolUserPackComponent extends ChatPack implements OnInit {
 
+  private mName: string;
+  private mTier: string;
+  private mDivision: string;
+  private mPoint: number;
+
   public constructor() {
     super();
+    this.mName = '';
+    this.mTier = '';
+    this.mDivision = '';
+    this.mPoint = 0;
   }
 
   public ngOnInit() {
+    const param = JSON.parse(this.message.getMessage()) as Param;
+
+    // console.log(param);
+    this.mName = param.name;
+    this.mTier = param.tier.tier;
+    this.mDivision = param.tier.division;
+    this.mPoint = param.tier.point;
   }
 
+  public getName(): string { return this.mName; }
+
+  public getTier(): string { return this.mTier; }
+
+  public getDivision(): string { return this.mDivision; }
+
+  public getPoint(): number { return this.mPoint; }
+
+}
+
+
+type Param = {
+  /*icon: "http://ddragon.leagueoflegends.com/cdn/10.10.3208608/img/profileicon/1298.png", "id": "PbjxOHtSve7VjlVuMU3HR9zDpwb-xKsD619EqiBJMuUUfw", "level": 146, "mmr": { "fow": 1111, "opgg": 1111 }, "most": { "rank": [], "top": [{ "background": "https://cdn.communitydragon.org/10.10.3208608/champion/Rumble/splash-art/skin/0", "icon": "http://ddragon.leagueoflegends.com/cdn/10.10.3208608/img/champion/Rumble.png", "name": "럼블" }, { "background": "https://cdn.communitydragon.org/10.10.3208608/champion/Sona/splash-art/skin/0", "icon": "http://ddragon.leagueoflegends.com/cdn/10.10.3208608/img/champion/Sona.png", "name": "소나" }, { "background": "https://cdn.communitydragon.org/10.10.3208608/champion/Lulu/splash-art/skin/0", "icon": "http://ddragon.leagueoflegends.com/cdn/10.10.3208608/img/champion/Lulu.png", "name": "룰루" }, { "background": "https://cdn.communitydragon.org/10.10.3208608/champion/Zyra/splash-art/skin/0", "icon": "http://ddragon.leagueoflegends.com/cdn/10.10.3208608/img/champion/Zyra.png", "name": "자이라" }, { "background": "https://cdn.communitydragon.org/10.10.3208608/champion/Elise/splash-art/skin/0", "icon": "http://ddragon.leagueoflegends.com/cdn/10.10.3208608/img/champion/Elise.png", "name": "엘리스" }] },*/
+  name: string,
+  tier: { division: "II", point: 21, tier: "SILVER" }
 }

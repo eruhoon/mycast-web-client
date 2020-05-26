e2927adee2a8ecc44580ad72704c04f1d6942584
Whimsicott <eruhoon@gmail.com>
2020-05-27 00:14:08 +0900

    implement mobile-page

--- src/app/views/mobile/mobile-page/mobile-page.component.ts
+++ src/app/views/mobile/mobile-page/mobile-page.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { MainService } from 'src/app/services/main/main.service';
+import { ThemeService } from 'src/app/services/theme/theme.service';
 
 @Component({
   selector: 'app-mobile-page',
   templateUrl: './mobile-page.component.html',
-  styleUrls: ['./mobile-page.component.scss']
+  styleUrls: ['./mobile-page.component.scss', './mobile-page.color.scss']
 })
 export class MobilePageComponent implements OnInit {
 
-  constructor() { }
+  public constructor(
+    private mMainSrv: MainService,
+    private mThemeSrv: ThemeService) { }
 
-  ngOnInit() {
+  public ngOnInit(): void {
   }
 
+  public toggleMenu(): void {
+    this.mMainSrv.toggleSidebar();
+  }
+
+  public isSideMenuShow(): boolean {
+    return this.mMainSrv.isSidebarShow();
+  }
 }

fefd5b22e0d38b205dbd3d3029b066b15747c955
Whimsicott <eruhoon@gmail.com>
2020-06-06 03:09:18 +0900

    bind ModifySettingModal

--- src/app/views/main-page/modify-setting-modal/modify-setting-modal.component.ts
+++ src/app/views/main-page/modify-setting-modal/modify-setting-modal.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NotificationSound } from 'src/app/models/notification/NotificationSound';
+import { NotificationSounds } from 'src/app/models/notification/NotificationSounds';
+import { OptionService } from 'src/app/services/option/option.service';
+import { ProfileModifyMode, ProfileService } from 'src/app/services/profile/profile.service';
 
 @Component({
   selector: 'modify-setting-modal',
@@ -7,9 +11,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModifySettingModalComponent implements OnInit {
 
-  constructor() { }
+  private mNotificationSounds: NotificationSounds;
+  private mNotificationSoundId: string;
 
-  ngOnInit() {
+  public constructor(
+    private mProfileSrv: ProfileService,
+    private mOptionSrv: OptionService) {
+    this.mNotificationSounds = new NotificationSounds();
+    this.mNotificationSoundId = NotificationSound.getDefaultSound().getId();
   }
 
+  public ngOnInit(): void {
+    this.mNotificationSoundId = this.mOptionSrv.getNotificationSound().getId();
+  }
+
+  public getNotficationSoundId(): string {
+    return this.mNotificationSoundId;
+  }
+
+  public getNotificationSounds(): NotificationSound[] {
+    return this.mNotificationSounds.getList();
+  }
+
+  public isDataSaveMode(): boolean {
+    return this.mOptionSrv.isDataSaveMode();
+  }
+
+  public toggleDataSaveMode(): void {
+    const option = this.mOptionSrv.isDataSaveMode();
+    return this.mOptionSrv.setDataSaveMode(!option);
+  }
+
+  public isTimestampShow(): boolean {
+    return this.mOptionSrv.isTimestampShow();
+  }
+
+  public toggleChatTimestamp(): void {
+    const option = this.mOptionSrv.isTimestampShow();
+    return this.mOptionSrv.setTimestampShow(!option);
+  }
+
+  public onNotificationSoundClick(option: NotificationSound): void {
+    const soundId = option.getId();
+    this.mNotificationSoundId = soundId;
+    this.mOptionSrv.setNotificationSound(soundId);
+  }
+
+  public close(): void {
+    this.mProfileSrv.setModifyMode(ProfileModifyMode.NONE);
+  }
 }

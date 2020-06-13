8e97b228f850818c93df020c0938669956eeee4c
Whimsicott <eruhoon@gmail.com>
2020-06-14 00:00:27 +0900

    bind Link in AnimationPack

--- src/app/views/chat/cp/animation-pack/animation-pack.component.ts
+++ src/app/views/chat/cp/animation-pack/animation-pack.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Injector } from '@angular/core';
 import { ChatPack } from '../ChatPack';
+import { LinkPopup } from 'src/app/models/link/LinkPopup';
+import { LinkPopupService } from 'src/app/services/link/link-popup.service';
 
 @Component({
   selector: 'animation-pack',
@@ -12,12 +14,16 @@ export class AnimationPackComponent extends ChatPack implements OnInit {
   public thumbnail: string;
   public date: string;
   public error: boolean;
+  private mLink: string;
 
-  public constructor(injector: Injector) {
+  public constructor(
+    injector: Injector,
+    private mLinkPopup: LinkPopupService) {
     super(injector);
     this.title = '';
     this.thumbnail = '';
     this.error = false;
+    this.mLink = '';
   }
 
   public ngOnInit() {
@@ -30,10 +36,15 @@ export class AnimationPackComponent extends ChatPack implements OnInit {
       this.title = raw.title;
       this.thumbnail = raw.thumbnail;
       this.date = raw.date;
+      this.mLink = raw.link;
     } catch (e) {
       this.error = true;
       console.error('fuck', e);
     }
+  }
+
+  public onClick(): void {
+    window.open(this.mLink, '_blank', 'width=800');
   }
 }
 

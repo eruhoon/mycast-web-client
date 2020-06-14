4d89e89144f3f8cceda268c17bc1ce18f6a8f430
Whimsicott <eruhoon@gmail.com>
2020-06-14 14:31:33 +0900

    add binding in CvPack

--- src/app/views/chat/cp/cv-pack/cv-pack.component.ts
+++ src/app/views/chat/cp/cv-pack/cv-pack.component.ts
@@ -8,11 +8,44 @@ import { ChatPack } from '../ChatPack';
 })
 export class CvPackComponent extends ChatPack implements OnInit {
 
+  public name: string;
+  public link: string;
+  public characters: CharacterParam[];
+
   public constructor(injector: Injector) {
     super(injector);
+    this.name = '';
+    this.link = '';
+    this.characters = [];
   }
 
   public ngOnInit() {
+    try {
+      const raw = JSON.parse(this.message.getMessage()) as Param;
+      this.bind(raw);
+    } catch (e) {
+      console.error(e);
+    }
   }
 
+  public onClick(): void {
+    window.open(this.link, '_blank', 'width=800');
+  }
+
+  private bind(raw: Param): void {
+    this.name = raw.name;
+    this.link = raw.link;
+    this.characters = raw.characters;
+  }
 }
+
+type Param = {
+  name: string,
+  link: string,
+  characters: CharacterParam[],
+};
+
+type CharacterParam = {
+  description: string,
+  icon: string,
+};

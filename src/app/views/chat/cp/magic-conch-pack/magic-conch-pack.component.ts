dbce6cf2de04843b8965c0462e661aa0f72bf450
Whimsicott <eruhoon@gmail.com>
2020-06-17 23:29:26 +0900

    bind param in MagicConchPack

--- src/app/views/chat/cp/magic-conch-pack/magic-conch-pack.component.ts
+++ src/app/views/chat/cp/magic-conch-pack/magic-conch-pack.component.ts
@@ -5,9 +5,12 @@ import { ChatPack } from '../ChatPack';
 @Component({
   selector: 'magic-conch-pack',
   templateUrl: './magic-conch-pack.component.html',
-  styleUrls: ['./magic-conch-pack.component.scss','./magic-conch-pack.color.scss']
+  styleUrls: ['./magic-conch-pack.component.scss', './magic-conch-pack.color.scss']
 })
 export class MagicConchPackComponent extends ChatPack implements OnInit {
+
+  public question: string;
+  public answer: string;
 
   public constructor(injector: Injector) {
     super(injector);
@@ -15,14 +18,10 @@ export class MagicConchPackComponent extends ChatPack implements OnInit {
 
   public ngOnInit() {
     try {
-      console.log(this.message);
-      console.log(this.message.getMessage());
-      console.log(this.message.getRequest());
-      const raw = JSON.parse(this.message.getMessage());
-      console.log(raw);
+      this.question = this.message.getRequest();
+      this.answer = this.message.getMessage();
     } catch (e) {
       console.error('parse error');
     }
   }
-
 }

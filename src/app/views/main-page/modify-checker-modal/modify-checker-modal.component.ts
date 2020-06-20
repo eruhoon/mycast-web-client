42fdb03cc31a2aa63647ae88bf5dfff7d28e1f3b
Whimsicott <eruhoon@gmail.com>
2020-06-20 12:27:33 +0900

    add binding platform in ModifyCheckerModal

--- src/app/views/main-page/modify-checker-modal/modify-checker-modal.component.ts
+++ src/app/views/main-page/modify-checker-modal/modify-checker-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService, ProfileModifyMode } from 'src/app/services/profile/profile.service';
 
 @Component({
   selector: 'modify-checker-modal',
@@ -7,13 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModifyCheckerModalComponent implements OnInit {
 
-  constructor() { }
+  public platforms: string[];
 
-  ngOnInit() {
+  public constructor(private mProfileSrv: ProfileService) {
+    this.platforms = [
+      'afreeca',
+      'twitch',
+      'youtube',
+      'mixer',
+    ];
+  }
+
+  public ngOnInit() {
   }
 
   public close() {
-
+    this.mProfileSrv.setModifyMode(ProfileModifyMode.NONE);
   }
-
 }

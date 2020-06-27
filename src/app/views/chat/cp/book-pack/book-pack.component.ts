7e5e68b7cc5a3f536dbc8ee5d2b9c8883a5b94f5
엘풍 <eruhoon@gmail.com>
2020-06-28 03:30:48 +0900

    [#44] bind param in BookPack

--- src/app/views/chat/cp/book-pack/book-pack.component.ts
+++ src/app/views/chat/cp/book-pack/book-pack.component.ts
@@ -1,3 +1,6 @@
+import { LinkPopupBuilder } from 'src/app/models/link/LinkPopupBuilder';
+import { LinkPopupService } from 'src/app/services/link/link-popup.service';
+
 import { Component, Injector, OnInit } from '@angular/core';
 
 import { ChatPack } from '../ChatPack';
@@ -9,11 +12,69 @@ import { ChatPack } from '../ChatPack';
 })
 export class BookPackComponent extends ChatPack implements OnInit {
 
-  public constructor(injector: Injector) {
+  public title: string;
+  public image: string;
+  public author: string;
+  public translator: string;
+  public publish: string;
+  public date: string;
+
+  private mLink: string;
+
+  public constructor(
+    injector: Injector,
+    private mLinkPopupSrv: LinkPopupService) {
+
     super(injector);
+    this.title = '';
+    this.image = '';
+    this.author = '';
+    this.translator = '';
+    this.publish = '';
+    this.date = '';
+    this.mLink = '';
   }
 
-  public ngOnInit() {
+  public ngOnInit(): void {
+    try {
+      const raw = JSON.parse(this.message.getMessage()) as Param;
+      this.bind(raw);
+      console.log(raw);
+    } catch {
+      console.error('fuck');
+    }
   }
 
+  public onClick(): void {
+    if (this.isMobile()) {
+      window.open(this.mLink, '_blank');
+    } else {
+      this.mLinkPopupSrv.addLink(new LinkPopupBuilder()
+        .title('Book Info')
+        .width(800).height(600)
+        .link(this.mLink)
+        .build());
+    }
+  }
+
+  private bind(raw: Param): void {
+    this.title = raw.title;
+    this.image = raw.thumbnail;
+    this.author = raw.author;
+    this.translator = raw.translator || '';
+    this.publish = raw.publish;
+    this.date = raw.date;
+    this.mLink = raw.link;
+  }
 }
+
+type Param = {
+  author: string,
+  category: string,
+  date: string,
+  link: string,
+  publish: string,
+  thumbnail: string,
+  title: string,
+  translator: string,
+};

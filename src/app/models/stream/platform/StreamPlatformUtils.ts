e074dd792295be5dfb0a1fda80dd2567b1f48c02
엘풍 <eruhoon@gmail.com>
2020-06-30 00:26:53 +0900

    add totoro in StreamPlatformUtils

--- src/app/models/stream/platform/StreamPlatformUtils.ts
+++ src/app/models/stream/platform/StreamPlatformUtils.ts
@@ -4,6 +4,7 @@ export class StreamPlatformUtils {
         const dir = `assets/image/stream`;
         switch (streamPlatform) {
             case 'local': return `/${dir}/mycast.png`;
+            case 'totoro':
             case 'twitch':
             case 'afreeca':
             case 'kakaotv':

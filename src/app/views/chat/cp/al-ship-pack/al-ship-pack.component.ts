2f4c283b8cb672ab71bd999d08b2af7c38662a53
엘풍 <eruhoon@gmail.com>
2020-06-30 22:42:10 +0900

    [#43] bind AzurLaneShip Pack

--- src/app/views/chat/cp/al-ship-pack/al-ship-pack.component.ts
+++ src/app/views/chat/cp/al-ship-pack/al-ship-pack.component.ts
@@ -1,3 +1,6 @@
+import { LinkPopupBuilder } from 'src/app/models/link/LinkPopupBuilder';
+import { LinkPopupService } from 'src/app/services/link/link-popup.service';
+
 import { Component, Injector, OnInit } from '@angular/core';
 
 import { ChatPack } from '../ChatPack';
@@ -9,15 +12,41 @@ import { ChatPack } from '../ChatPack';
 })
 export class AlShipPackComponent extends ChatPack implements OnInit {
 
-  public constructor(injector: Injector) {
+  public prop: Param;
+
+  public constructor(
+    injector: Injector,
+    private mLinkPopupSrv: LinkPopupService) {
     super(injector);
   }
 
   public ngOnInit() {
-    console.log(this.message);
+    const raw = JSON.parse(this.message.getMessage());
+    this.prop = {
+      icon: raw.icon,
+      link: raw.link,
+      title: raw.name,
+      subtitle: `${raw.rarity} ${raw.type.name}`,
+    };
   }
 
   public onClick(): void {
+    if (this.isMobile()) {
+      window.open(this.prop.link, '_blank');
+    } else {
+      this.mLinkPopupSrv.addLink(new LinkPopupBuilder()
+        .title('Item Info')
+        .width(800).height(600)
+        .link(this.prop.link)
+        .build());
+    }
     console.log(this.message);
   }
 }
+
+type Param = {
+  title: string,
+  subtitle: string,
+  icon: string,
+  link: string,
+};

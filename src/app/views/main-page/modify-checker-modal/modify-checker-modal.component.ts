7552418b21f9092bc11edd05b3c38c91c7ff4605
Whimsicott <eruhoon@gmail.com>
2020-07-04 16:16:09 +0900

    [#53] bind event ModifyCheckerModal

--- src/app/views/main-page/modify-checker-modal/modify-checker-modal.component.ts
+++ src/app/views/main-page/modify-checker-modal/modify-checker-modal.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { ProfileService, ProfileModifyMode } from 'src/app/services/profile/profile.service';
+
+import { MutableStream } from 'src/app/models/stream/MutableStream';
+import { Stream } from 'src/app/models/stream/Stream';
 
 @Component({
   selector: 'modify-checker-modal',
@@ -8,21 +11,52 @@ import { ProfileService, ProfileModifyMode } from 'src/app/services/profile/prof
 })
 export class ModifyCheckerModalComponent implements OnInit {
 
-  public platforms: string[];
+  @ViewChild('searchName', { static: false })
+  public mSearchInput: ElementRef<HTMLInputElement>;
+  public platforms: PlatformParam[];
+  public currentPlatform: PlatformParam | null;
+  public stagedStream: Stream | null;
 
   public constructor(private mProfileSrv: ProfileService) {
     this.platforms = [
-      'afreeca',
-      'twitch',
-      'youtube',
-      'mixer',
+      { name: '트위치', icon: '/assets/image/stream/twitch.png' },
+      { name: '아프리카', icon: '/assets/image/stream/afreeca.png' },
+      // { name: '유튜브', icon: '/assets/image/stream/youtube.png' },
+      // { name: '카카오TV', icon: '/assets/image/stream/kakaotv.png' },
     ];
+    this.currentPlatform = null;
+    this.stagedStream = null;
   }
 
   public ngOnInit() {
+  }
+
+  public search(): void {
+    const searchName = this.mSearchInput.nativeElement.value;
+    const searchPlatform = this.currentPlatform;
+    this.stagedStream = this.createMockStream();
+    console.log('search', searchName, searchPlatform);
+  }
+
+  private createMockStream(): Stream {
+    const stream = new MutableStream();
+    stream.setIcon('https://static-cdn.jtvnw.net/jtv_user_pictures/89e29e2e-f165-40e6-bc0c-d42205935216-profile_image-300x300.png');
+    stream.setTitle('침착맨');
+    stream.setKeyId('hello');
+    stream.setPlatform('twitch');
+    return stream;
+  }
+
+  public selectPlatform(platform: PlatformParam): void {
+    this.currentPlatform = platform;
   }
 
   public close() {
     this.mProfileSrv.setModifyMode(ProfileModifyMode.NONE);
   }
 }
+
+type PlatformParam = {
+  name: string,
+  icon: string,
+};
